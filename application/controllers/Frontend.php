a08486c4cf850b029b4753bf1c1422fd14b47de0
hgagliardi <hg.gagliardi@gmail.com>
2017-01-24 16:03:41 -0300

    Cambios en get posts

--- application/controllers/Frontend.php
+++ application/controllers/Frontend.php
@@ -7,9 +7,13 @@ class Frontend extends CI_Controller {
     $this->load->view('frontend/index');
   }
 
-  public function get_posts(){
+  public function get_posts($start=0, $limit=5){
+
     $this->db->from('posts');
     $this->db->select('*');
+    $this->db->limit($limit, $start);
+    $this->db->join('categories_posts', 'categories_posts.post_id = posts.posts_id');
+    $this->db->group_by('post_id');
 
     $query = $this->db->get();
 
