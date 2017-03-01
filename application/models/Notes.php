c482740fffa2f134ee71a2fe366a03b52b323f55
hgagliardi <hg.gagliardi@gmail.com>
2017-03-01 10:28:36 -0300

    notas as text

--- application/models/Notes.php
+++ application/models/Notes.php
@@ -2,7 +2,7 @@
 
 Class Notes extends CI_Model {
     public function create($title, $content,$date){
-        $sql = "INSERT INTO notes (title,content,date) 
+        $sql = "INSERT INTO notes (title,content,date)
         VALUES ('$title','$content','$date')";
 
         $this->db->query($sql);
@@ -12,24 +12,24 @@ Class Notes extends CI_Model {
             return $this->db->affected_rows();
         }
     }
-    
+
     public function get($note_id = null){
         $this->db->from('notes');
         if($note_id != null){
             $this->db->where('notes_id', $note_id);
         }
-        $this->db->select("notes_id,title,SUBSTRING(content,1,30) as content,image");
-      
-       
+        $this->db->select("notes_id,title,SUBSTRING(content,1,30) as text,image");
+
+
         $query = $this->db->get();
-        
+
         if ($query->num_rows() > 0) {
             return $query->result();
         } else {
             return array();
         }
     }
-    
+
     public function get_notes($limit, $start){
         $this->db->from('notes');
         if($note_id != null){
@@ -37,49 +37,49 @@ Class Notes extends CI_Model {
         }
         $this->db->limit($limit, $start);
         $this->db->select("notes_id,title,SUBSTRING(content,1,30) as content,image");
-      
-       
+
+
         $query = $this->db->get();
-        
+
         if ($query->num_rows() > 0) {
             return $query->result();
         } else {
             return array();
         }
     }
-    
+
     public function update($note_id,$title, $content){
         $sql = "UPDATE notes SET  "
                 . " title = '$title',  content = '$content'
         WHERE notes_id=$note_id";
-         
+
         $this->db->query($sql);
-        
+
         if($this->db->affected_rows() > 0){
             return 1;
         }else{
-            
+
             return 0;
         }
     }
-    
+
     public function set_img($note_id,$img_name){
          $sql = "UPDATE notes SET  "
                 . " image = '$img_name'
         WHERE notes_id=$note_id";
-         
+
         $this->db->query($sql);
-        
+
         if($this->db->affected_rows() > 0){
             return 1;
         }else{
-            
+
             return 0;
         }
     }
-    
+
     public function set_category($note_id,$category_id){
-        $sql = "INSERT INTO categories_notes (note_id,category_id) 
+        $sql = "INSERT INTO categories_notes (note_id,category_id)
         VALUES ($note_id,$category_id)";
 
         $this->db->query($sql);
@@ -89,17 +89,17 @@ Class Notes extends CI_Model {
             return $this->db->affected_rows();
         }
     }
-    
+
     public function get_categories($note_id){
         $this->db->from('categories_notes');
-        
+
         $this->db->where('note_id', $note_id);
-        
+
         $this->db->select('category_id');
-      
-       
+
+
         $query = $this->db->get();
-        
+
         if ($query->num_rows() > 0) {
             return $query->result_array();
         } else {
--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1">
 
-    <title>ReactJS and ES6</title>
+    <title>Juan Patricio Mussi</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
@@ -13,18 +13,22 @@
 
     <link rel="stylesheet" href="<?php echo base_url('assets/');?>/css/todo.css" media="screen" title="no title" charset="utf-8">
 
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7dNJE7C3mPlWhcHpTGsh1QJNyjfVa5BY"></script>
+
 </head>
 <body>
 
 <div id="app"></div>
 
+<div id="map"></div>
+
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-<!-- <script src="./node_modules/readmore-js/readmore.min.js"></script> -->
-<!-- <script type="text/javascript" src="./dist/js/expandirArticulo.js"></script> -->
-<script src="<?php echo base_url('assets/');?>/js/bundle.js"></script>
 
 </body>
+<script src="<?php echo base_url('assets/');?>/js/bundle.js"></script>
+<script src="<?php echo base_url('assets/');?>/js/bundlemap.js"></script>
 <script src="<?php echo base_url('assets/') ?>/js/app.js"></script>
+
 </html>
